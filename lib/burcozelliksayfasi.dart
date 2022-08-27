6f488cc6d2366a38243cb7cc255e3a781d19a7d7
Süleyman Batuhan DALKILIÇ <batuhandalkilic@outlook.com>
2022-08-27 19:56:43 +0300

    palette_generator package eklendi

--- lib/burcozelliksayfasi.dart
+++ lib/burcozelliksayfasi.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import './data/strings.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 // her bir sayfa için 1 tane yaptık verilere göre şekilleniyor!
-class BurcPage extends StatelessWidget {
+class BurcPage extends StatefulWidget {
   final String buyukresimler;
   final String burcozellikleri;
   final List<String> burclist;
@@ -15,22 +16,36 @@ class BurcPage extends StatelessWidget {
       required this.buyukresimler});
 
   @override
+  State<BurcPage> createState() => _BurcPageState();
+}
+
+class _BurcPageState extends State<BurcPage> {
+  Color appbarRengi = Colors.transparent;
+  late PaletteGenerator
+      _generator; // ben sana değer vereceğim merak etme = late/ initialize yapacağım
+  @override
+  void initState() {
+    super.initState();
+    appbarRenginiBul();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: CustomScrollView(
           slivers: [
             SliverAppBar(
-                backgroundColor: Colors.red,
+                backgroundColor: appbarRengi,
                 floating: true,
                 pinned: true,
                 expandedHeight: 200,
                 flexibleSpace: FlexibleSpaceBar(
                   expandedTitleScale: 1.2,
                   centerTitle: true,
-                  title: Text("$burcadi Burcu ve Özellikleri"),
+                  title: Text("${widget.burcadi} Burcu ve Özellikleri"),
                   background: Image.asset(
-                    "assets/images/$buyukresimler",
+                    "assets/images/${widget.buyukresimler}",
                     fit: BoxFit.cover,
                   ),
                 )),
@@ -42,7 +57,7 @@ class BurcPage extends StatelessWidget {
                   child: Padding(
                     padding: const EdgeInsets.symmetric(vertical: 90),
                     child: Text(
-                      burcozellikleri,
+                      widget.burcozellikleri,
                       style: TextStyle(letterSpacing: 1),
                     ),
                   ),
@@ -53,5 +68,14 @@ class BurcPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> appbarRenginiBul() async {
+    _generator = await PaletteGenerator.fromImageProvider(
+        AssetImage("assets/images/${widget.buyukresimler}"));
+
+    setState(() {
+      appbarRengi = _generator.dominantColor!.color;
+    });
   }
 }
